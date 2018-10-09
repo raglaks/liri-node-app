213a9d63c5f6f50d937d052ec0a7dba265f0f4c3
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-10-09 18:18:53 -0500

    fixed bug that wouldn't allow for inputs with more than one word

--- liri.js
+++ liri.js
@@ -1,16 +1,28 @@
 require("dotenv").config();
 
-const keys = require("./keys.js");
+// const keys = require("./keys.js");
 let request = require("request");
 let moment = require("moment");
 
 // const spotify = new Spotify(keys.spotify);
 
+let args = process.argv;
+let queryArray = [];
+
+for (i=3; i < args.length; i++) {
+    let toPush = args[i]
+    queryArray.push(toPush);
+}
+
+let string = queryArray.join(" ");
+
+console.log(args);
+console.log(string);
+
 let command = process.argv[2];
-let input = process.argv[3];
 
 if (command === "concert-this") {
-    if (input) {
+    if (string) {
         concertThis();
     } else {
         console.log("please input a band name after the command");
@@ -18,7 +30,11 @@ if (command === "concert-this") {
 } else if (command === "spotify-this-song") {
     console.log("spotify-this-song");
 } else if (command === "movie-this") {
-    console.log("movie-this");
+    if (string) {
+        movieTHis();
+    } else {
+        console.log("please input a movie name after the command");
+    }
 } else if (command === "do-what-it-says") {
     console.log("do-what-it-says");
 } else {
@@ -26,24 +42,27 @@ if (command === "concert-this") {
 }
 
 function concertThis() {
-    console.log(input);
 
-    request(`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`, function (error, response, body) {
+    request(`https://rest.bandsintown.com/artists/${string}/events?app_id=codingbootcamp`, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             let stringObj = JSON.parse(body);
 
             stringObj.forEach(element => {
-                
+
                 let venue = element.venue.name;
                 let city = element.venue.city;
                 let date = element.datetime;
                 let format = moment(date).format("DD/MM/YYYY");
 
-                console.log(`${input} is playing a show in ${city}, at the ${venue}, on ${format}.`);
+                console.log(`${string} are playing a show in ${city}, at ${venue}, on ${format}.`);
             });
 
         } else {
             console.log("something's wrong");
         }
     });
+}
+
+function movieTHis() {
+
 }
