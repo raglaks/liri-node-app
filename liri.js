993c57cc00f75fd511ea9c3c85ea2b5ffad59ef9
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-10-09 13:40:14 -0500

    added necessary node modules; also added basic script to check user command

--- liri.js
+++ liri.js
@@ -1,7 +1,19 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const keys = require("keys");
+// const keys = require("keys");
 
-const spotify = new Spotify(keys.spotify);
+// const spotify = new Spotify(keys.spotify);
 
-console.log(process.argv);
+let command = process.argv[2];
+
+if (command === "concert-this") {
+    console.log("concert-this");
+} else if (command === "spotify-this-song") {
+    console.log("spotify-this-song");
+} else if (command === "movie-this") {
+    console.log("movie-this");
+} else if (command === "do-what-it-says") {
+    console.log("do-what-it-says");
+} else {
+    console.log("please input one of the available commands:\nconcert-this\nspotify-this-song\nmovie-this\ndo-what-it-says");
+}
