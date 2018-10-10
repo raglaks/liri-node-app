e540046c737b9507bde9ac3004b42ee3878f1442
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-10-10 14:48:58 -0500

    finished logic for spotify-this song function

--- liri.js
+++ liri.js
@@ -1,13 +1,12 @@
 //setting environment using require module and .env file (and dotenv module)
 require("dotenv").config();
 
-// const keys = require("./keys.js");
-
 //require request module and moment module for usage in app
-let request = require("request");
-let moment = require("moment");
+const request = require("request");
+const moment = require("moment");
+const spotify = require('node-spotify-api');
+const keys = require("./keys");
 
-// const spotify = new Spotify(keys.spotify);
 
 //var for process.argv array
 let args = process.argv;
@@ -15,7 +14,7 @@ let args = process.argv;
 let queryArray = [];
 
 //for loop to get each word of user input and then push it to the queryArray string
-for (i=3; i < args.length; i++) {
+for (i = 3; i < args.length; i++) {
     let toPush = args[i]
     queryArray.push(toPush);
 }
@@ -27,23 +26,37 @@ let string = queryArray.join(" ");
 let command = process.argv[2];
 
 if (command === "concert-this") {
+
     if (string) {
         concertThis();
     } else {
         console.log("please input a band name after the command");
     }
+
 } else if (command === "spotify-this-song") {
-    console.log("spotify-this-song");
+
+    if (string) {
+        spotifyThis();
+    } else {
+        console.log("please input a song name after the command");
+    }
+
 } else if (command === "movie-this") {
+
     if (string) {
         movieTHis();
     } else {
         console.log(`If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/\nit's on Netflix also`);
     }
+
 } else if (command === "do-what-it-says") {
+
     console.log("do-what-it-says");
+
 } else {
+
     console.log("please input one of the available commands:\nconcert-this\nspotify-this-song\nmovie-this\ndo-what-it-says");
+
 }
 
 function concertThis() {
@@ -69,7 +82,7 @@ function concertThis() {
 }
 
 function movieTHis() {
-    request(`http://www.omdbapi.com/?t=${string}&y=&plot=short&apikey=trilogy`, function (error, response, body){
+    request(`http://www.omdbapi.com/?t=${string}&y=&plot=short&apikey=trilogy`, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             let stringObj = JSON.parse(body);
 
@@ -88,5 +101,37 @@ function movieTHis() {
             console.log("something's wrong, try again");
         }
     })
-   
+
+}
+
+function spotifyThis() {
+
+    let getKeys = new spotify(keys.spotify);
+
+    console.log(getKeys);
+
+    getKeys.search({ type: 'track', query: `${string}`, limit: 3 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        //This will show the following information about the song in your terminal/bash window:
+        // Artist(s)
+        // The song's name
+        // A preview link of the song from Spotify
+        // The album that the song is from
+
+        //name of artist
+        console.log(data.tracks.items[0].artists[0].name);
+
+        //song name
+        console.log(data.tracks.items[0].name);
+
+        //album
+        console.log(data.tracks.items[0].album.name);
+
+        //preview
+        console.log(data.tracks.items[0].external_urls.spotify);
+    });
+
 }
